ee156f44e9401d7399a83bf5bde9764fac0aa483
Juliogs1999 <63380496+Juliogs1999@users.noreply.github.com>
2023-10-23 19:26:44 -0500

    23-10-2023

--- ClinicaDental-web/js/main.js
+++ ClinicaDental-web/js/main.js
@@ -1,13 +1,22 @@
-$(function(){
-    $("#item-menu-nosotros").click(function(e){
+$(function () {
+    $("#item-menu-nosotros").click(function (e) {
         e.preventDefault();
         fetch('./nosotros.html')
-            .then(function(response) {
+            .then(function (response) {
                 return response.text();
             })
-            .then(function(datoshtml) {
-                //console.log(datoshtml);
+            .then(function (datoshtml) {
                 $("#main-content").html(datoshtml);
             });
-    })
-})
+    });
+
+    $(document).ready(function () {
+        $('#seleccionarHorario').on('click', function () {
+            $('#horario').datetimepicker({
+                format: 'LT', // Formato de 12 horas con AM/PM
+                stepping: 60,  // Incremento de minutos
+            });
+            $('#horario').datetimepicker('show');
+        });
+    });
+});
